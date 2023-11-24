a98164cfcf0d153d4d6ec1499b68e6ee3042dadc
Arian van Putten <arian.vanputten@gmail.com>
2023-11-24 13:52:44 +0000

    add devShell

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "A very basic flake";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
   outputs = { self, nixpkgs }:
     let inherit (nixpkgs) lib; in
 
@@ -19,6 +23,12 @@
 
       checks = lib.genAttrs self.lib.supportedSystems (system: {
         resizePartition = nixpkgs.legacyPackages.${system}.nixosTest ./tests/resize-partition.nix;
+      });
+
+      devShells = lib.genAttrs self.lib.supportedSystems (system: {
+        default = let pkgs = nixpkgs.legacyPackages.${system}; in pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.awscli2 pkgs.opentofu ];
+        };
       });
 
     };
