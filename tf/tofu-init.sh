fe82cc6caa7db97537a3b7e78f520cba8436535e
Arian van Putten <arian.vanputten@gmail.com>
2023-11-29 11:28:49 +0000

    fix

--- tf/tofu-init.sh
+++ tf/tofu-init.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 tofu init \
     -backend-config="bucket=$(cd state-backend; tofu output -raw bucket)" \
     -backend-config="region=$(cd state-backend; tofu output -raw region)" \
-    -backend-config="dynamodb_table=$(cd state-backend; tofu -raw dynamodb_table)" \
+    -backend-config="dynamodb_table=$(cd state-backend; tofu output -raw dynamodb_table)" \
     "$@"
