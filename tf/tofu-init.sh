bc163dbeb6e504b3bfacdeb5aff660da64a64ed2
Arian van Putten <arian.vanputten@gmail.com>
2023-12-02 11:49:26 +0100

    Fix tofu

--- tf/tofu-init.sh
+++ tf/tofu-init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+(cd state-backend; tofu init)
+
 tofu init \
     -backend-config="bucket=$(cd state-backend; tofu output -raw bucket)" \
     -backend-config="region=$(cd state-backend; tofu output -raw region)" \
