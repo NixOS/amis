74e44269cb9122295a3599a559fe2546be0b0e6a
Arian van Putten <arian.vanputten@gmail.com>
2023-12-02 11:40:50 +0100

    Fix aarch64

--- flake.nix
+++ flake.nix
@@ -5,17 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ... }:
     let inherit (nixpkgs) lib; in
 
     {
       nixosModules = {
         ec2-instance-connect = ./modules/ec2-instance-connect.nix;
-     
+
         legacyAmazonProfile = nixpkgs + "nixos/modules/virtualisation/amazon-image.nix";
         legacyAmazonImage = nixpkgs + "/nixos/maintainers/scripts/ec2/amazon-image.nix";
 
-	amazonProfile = ./modules/amazon-profile.nix;
+        amazonProfile = ./modules/amazon-profile.nix;
         amazonImage = ./modules/amazon-image.nix;
 
         mock-imds = ./modules/mock-imds.nix;
@@ -27,7 +27,7 @@
         };
       };
 
-      lib.supportedSystems = [ "x86_64-linux" ];
+      lib.supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
 
       packages = lib.genAttrs self.lib.supportedSystems (system:
         let pkgs = nixpkgs.legacyPackages.${system};
@@ -37,6 +37,7 @@
           amazon-ec2-metadata-mock = pkgs.buildGoModule rec {
             pname = "amazon-ec2-metadata-mock";
             version = "1.11.2";
+            doCheck = false; # check is flakey
             src = pkgs.fetchFromGitHub {
               owner = "aws";
               repo = "amazon-ec2-metadata-mock";
@@ -46,37 +47,34 @@
             vendorHash = "sha256-T45abGVoiwxAEO60aPH3hUqiH6ON3aRhkrOFcOi+Bm8=";
           };
 
+          amazonImage = (nixpkgs.lib.nixosSystem {
+            specialArgs.selfPackages = self.packages.${system};
+            pkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+              self.nixosModules.ec2-instance-connect
+              self.nixosModules.amazonImage
+              self.nixosModules.version
+            ];
+          }).config.system.build.amazonImage;
+          legacyAmazonImage = (lib.nixosSystem {
+            specialArgs.selfPackages = self.packages.${system};
+            pkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+              self.nixosModules.legacyAmazonImage
+              {
+                boot.loader.grub.enable = false;
+                boot.loader.systemd-boot.enable = true;
+              }
+              { ec2.efi = true; amazonImage.sizeMB = "auto"; }
+              self.nixosModules.version
+            ];
+          }).config.system.build.amazonImage;
+
         });
 
-      nixosConfigurations = {
-        amazonImage-x64_64-linux = lib.nixosSystem rec {
-          specialArgs.selfPackages = self.packages.${system};
-          pkgs = nixpkgs.legacyPackages.${system};
-          system = "x86_64-linux";
-          modules = [
-            self.nixosModules.ec2-instance-connect
-            self.nixosModules.amazonImage
-            self.nixosModules.version
-          ];
-        };
-        legacyAmazonImage-x86_64-linux = lib.nixosSystem rec {
-          specialArgs.selfPackages = self.packages.${system};
-          pkgs = nixpkgs.legacyPackages.${system};
-          system = "x86_64-linux";
-          modules = [
-            self.nixosModules.legacyAmazonImage
-            {
-              boot.loader.grub.enable = false;
-              boot.loader.systemd-boot.enable = true;
-            }
-            { ec2.efi = true; amazonImage.sizeMB = "auto"; }
-            self.nixosModules.version
-          ];
-        };
-      };
 
-
-      checks = lib.genAttrs self.lib.supportedSystems (system:
+      # TODO: unfortunately I don't have access to a aarch64-linux hardware with virtualisation support
+      checks = lib.genAttrs [ "x86_64-linux" ] (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           config = {
