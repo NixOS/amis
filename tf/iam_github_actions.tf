bcfbfa125bcd5b3de273562ab6e7e23e21b053d9
Arian van Putten <arian.vanputten@gmail.com>
2023-12-15 18:14:54 +0100

    plan and apply in github actions

--- tf/iam_github_actions.tf
+++ tf/iam_github_actions.tf
@@ -13,6 +13,40 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
 
+data "aws_iam_policy_document" "assume_plan" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = [local.audience]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.repo}:pull_request"]
+    }
+  }
+}
+
+resource "aws_iam_role" "plan" {
+  name                = "plan"
+  assume_role_policy  = data.aws_iam_policy_document.assume_plan.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+}
+
+output "plan_role_arn" {
+  value = aws_iam_role.plan.arn
+}
+
 data "aws_iam_policy_document" "assume_deploy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -32,7 +66,7 @@ data "aws_iam_policy_document" "assume_deploy" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.repo}:ref:refs/heads/main"]
+      values   = ["repo:${var.repo}:environment:infra"]
     }
   }
 }
@@ -48,10 +82,37 @@ output "deploy_role_arn" {
 }
 
 
+data "aws_iam_policy_document" "assume_upload_ami" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = [local.audience]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:${var.repo}:environment:images",
+        "repo:${var.repo}:environment:amis",
+        ]
+    }
+  }
+}
+
 # TODO: Tighten rules
 resource "aws_iam_role" "upload_ami" {
   name                = "upload-ami"
-  assume_role_policy  = data.aws_iam_policy_document.assume_deploy.json
+  assume_role_policy  = data.aws_iam_policy_document.assume_upload_ami.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
