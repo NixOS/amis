1d521e53c177ee17e86c7aa04a96bc480b21224d
Arian van Putten <arian.vanputten@gmail.com>
2023-12-17 13:54:55 +0100

    Restrict IAM further

--- tf/iam_github_actions.tf
+++ tf/iam_github_actions.tf
@@ -1,7 +1,3 @@
-locals {
-  audience = "sts.amazonaws.com"
-}
-
 variable "repo" {
   type    = string
   default = "arianvp/amis"
@@ -9,14 +5,33 @@ variable "repo" {
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = [local.audience]
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
 
-data "aws_iam_policy_document" "assume_plan" {
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "assume_AdministratorAccess" {
   statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.arn]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "assume_plan" {
+  source_policy_documents = [data.aws_iam_policy_document.assume_AdministratorAccess.json]
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
@@ -26,37 +41,37 @@ data "aws_iam_policy_document" "assume_plan" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
-      values   = [local.audience]
+      values   = aws_iam_openid_connect_provider.github_actions.client_id_list
     }
 
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = [
+      values = [
         "repo:${var.repo}:pull_request",
         "repo:${var.repo}:ref:refs/heads/main",
-        ]
+      ]
     }
   }
 }
 
 data "aws_iam_policy_document" "state" {
   statement {
+    effect = "Allow"
     actions = [
       "dynamodb:DescribeTable",
       "dynamodb:GetItem",
       "dynamodb:PutItem",
       "dynamodb:DeleteItem",
     ]
-    effect    = "Allow"
     resources = [data.terraform_remote_state.state_backend.outputs.dynamodb_table_arn]
   }
   statement {
+    effect = "Allow"
     actions = [
       "s3:ListBucket",
       "s3:GetObject",
     ]
-    effect    = "Allow"
     resources = [data.terraform_remote_state.state_backend.outputs.bucket_arn]
 
   }
@@ -82,9 +97,10 @@ output "plan_role_arn" {
 }
 
 data "aws_iam_policy_document" "assume_deploy" {
+  source_policy_documents = [data.aws_iam_policy_document.assume_AdministratorAccess.json]
   statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
@@ -94,7 +110,7 @@ data "aws_iam_policy_document" "assume_deploy" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
-      values   = [local.audience]
+      values   = aws_iam_openid_connect_provider.github_actions.client_id_list
     }
 
     condition {
@@ -116,10 +132,12 @@ output "deploy_role_arn" {
 }
 
 
+
 data "aws_iam_policy_document" "assume_upload_ami" {
+  source_policy_documents = [data.aws_iam_policy_document.assume_AdministratorAccess.json]
   statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
@@ -129,25 +147,53 @@ data "aws_iam_policy_document" "assume_upload_ami" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
-      values   = [local.audience]
+      values   = aws_iam_openid_connect_provider.github_actions.client_id_list
     }
 
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values = [
-        "repo:${var.repo}:environment:images",
-        "repo:${var.repo}:environment:amis",
-      ]
+      values   = ["repo:${var.repo}:environment:images"]
     }
   }
 }
 
-# TODO: Tighten rules
+data "aws_iam_policy_document" "upload_ami" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:HeadObject",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.images.arn}/*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:ImportSnapshot",
+      "ec2:DescribeImportSnapshotTasks",
+      "ec2:DescribeSnapshots",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeImages",
+      "ec2:RegisterImage",
+      "ec2:DeregisterImage",
+      "ec2:DescribeRegions",
+      "ec2:CopyImage",
+      "ec2:ModifyImageAttribute",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "upload_ami" {
+  name   = "upload-ami"
+  policy = data.aws_iam_policy_document.upload_ami.json
+}
+
 resource "aws_iam_role" "upload_ami" {
   name                = "upload-ami"
   assume_role_policy  = data.aws_iam_policy_document.assume_upload_ami.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  managed_policy_arns = [aws_iam_policy.upload_ami.arn]
 }
 
 output "upload_ami_role_arn" {
