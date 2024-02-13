994aff2282a31c2bf89f180f64c28e8e6b2a3f42
Arian van Putten <arian.vanputten@gmail.com>
2024-02-13 21:53:19 +0100

    Make GitHub pages (#68)
    * WIP Site
    
    * hmm
    
    * quadratic but fine for now
    
    * try upload
    
    * Run parallel
    
    * Run parallel
    
    * Run parallel
    
    * Apply suggestions from code review
    
    * Separate action
    
    * upload locks

---
+++ upload-ami/src/upload_ami/describe_images.py
@@ -0,0 +1,24 @@
+import logging
+import boto3
+import json
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    ec2 = boto3.client("ec2")
+    regions = ec2.describe_regions()["Regions"]
+
+    images = {}
+
+    for region in regions:
+        ec2r = boto3.client("ec2", region_name=region["RegionName"])
+
+        result = ec2r.describe_images(
+            Owners=["self"],
+            ExecutableUsers=["all"],
+        )
+        images[region["RegionName"]] = result["Images"]
+    
+    print(json.dumps(images, indent=2))
+
+if __name__ == "__main__":
+    main()
