8e1456a994179990a9ca8c4ec6fba1bcba88deba
Arian van Putten <arian.vanputten@gmail.com>
2024-03-02 20:02:58 +0100

    Fix assuming roles for debugging. these roles are named differently outside of us-east-1

--- tf/assume_administrator_access_policy_document/main.tf
+++ tf/assume_administrator_access_policy_document/main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+variable "sso_region" {
+  type    = string
+  default = "eu-north-1"
+}
+
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "this" {
@@ -20,7 +25,11 @@ data "aws_iam_policy_document" "this" {
     condition {
       test     = "ArnLike"
       variable = "aws:PrincipalArn"
-      values   = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_*"]
+      values = [
+        var.sso_region == "us-east-1" ?
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSAdministratorAccess_*" :
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${var.sso_region}/AWSReservedSSO_AWSAdministratorAccess_*"
+      ]
     }
   }
 }
