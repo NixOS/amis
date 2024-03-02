711aed4de63d1772dfcb519c3234d2c9cf6ec340
Arian van Putten <arian.vanputten@gmail.com>
2024-03-02 20:44:28 +0100

    Fix smoketest terminating terminated instances

--- upload-ami/src/upload_ami/smoke_test.py
+++ upload-ami/src/upload_ami/smoke_test.py
@@ -55,7 +55,8 @@ def smoke_test(image_id, run_id, cancel):
         raise
     finally:
         logging.info(f"Terminating instance {instance_id}")
-        ec2.terminate_instances(InstanceIds=[instance_id])
+        if instance["State"]["Name"] != "terminated":
+            ec2.terminate_instances(InstanceIds=[instance_id])
         ec2.get_waiter("instance_terminated").wait(InstanceIds=[instance_id])
 
 
