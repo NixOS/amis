64332805dc1c6e8864125b1096642247c73026f4
Arian van Putten <arian.vanputten@gmail.com>
2024-04-19 18:58:52 +0200

    Should allow deletes

--- tf/iam_github_actions.tf
+++ tf/iam_github_actions.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "upload_ami" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
+      "s3:DeleteObject",
     ]
     resources = ["${aws_s3_bucket.images.arn}/*"]
   }
