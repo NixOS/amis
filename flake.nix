4c54158814669aa545bb84e53609a12c7823f342
Arian van Putten <arian.vanputten@gmail.com>
2026-02-17 10:29:56 +0100

    Build image in ci (#335)
    * build image in cI
    
    * update README
    
    * Update README
    
    * run checks on linux
    
    * fix issues
    
    * fmt
    
    * Disable magic nix cache for now
    
    * only build on x86 and no magic-nix-cache
    
    * last try. fix ci. the loop continues
    
    * another one flakes the dust
    
    * Try with cachix installer to reproduce

--- flake.nix
+++ flake.nix
@@ -19,11 +19,14 @@
     let
       inherit (nixpkgs) lib;
       inherit (lib) genAttrs mapAttrs;
-      supportedSystems = [
+      linuxSystems = [
         "aarch64-linux"
         "x86_64-linux"
-        "aarch64-darwin"
       ];
+      supportedSystems = [
+        "aarch64-darwin"
+      ]
+      ++ linuxSystems;
       eachSystem = f: genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
       treefmtEval = eachSystem (
         pkgs:
@@ -63,12 +66,31 @@
         mapAttrs mkApp self.packages.${system}.upload-ami.passthru.upload-ami.pyproject.project.scripts
       );
 
+      # NOTE: We don't build the production images with these (yet). We use a hydra job instead
+      # NOTE: Github Actions doesn't support kvm on arm64 builds
+      nixosConfigurations.x86_64-linux = nixpkgs.lib.nixosSystem {
+        modules = [
+          (
+            { modulesPath, ... }:
+            {
+              imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+              nixpkgs.hostPlatform = "x86_64-linux";
+              system.stateVersion = "26.05";
+            }
+          )
+        ];
+      };
+
       formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
-      checks = genAttrs supportedSystems (system: {
-        inherit (self.packages.${system}) upload-ami;
-        formatting = treefmtEval.${system}.config.build.check self;
-      });
+      checks =
+        genAttrs linuxSystems (system: {
+          inherit (self.packages.${system}) upload-ami;
+          formatting = treefmtEval.${system}.config.build.check self;
+        })
+        // {
+          x86_64-linux.system = self.nixosConfigurations.x86_64-linux.config.system.build.images.amazon;
+        };
 
       devShells = genAttrs supportedSystems (system: {
         default = self.packages.${system}.upload-ami;
